66b69fb2c940f9c2a37d6815ab5a4d3e1ca9b176
Huazai <huaz@apricot.one>
2022-11-18 14:30:15 +0800

    remove dependency of sdk-ts

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,5 +1,5 @@
-import { getRPCConnection } from '@apricot-lend/sdk-ts';
+import { Connection } from '@solana/web3.js';
 
-const connection = getRPCConnection('triton');
+const connection = new Connection((process.env.RPC_TRITON_URL as string) || process.env.RPC as string || 'https://api.mainnet-beta.solana.com');
 
 export default connection;
